3590e75b21acac512fd55a770cd3846f431aefe2
Umar Mohammed <umar.mohammed@holisticai.com>
2024-02-02 15:58:53 +0000

    add shadcn

--- packages/web/src/Test.tsx
+++ packages/web/src/Test.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
 import { trpc } from "./utils/trpc";
 
 export function Test() {
   const { data } = trpc.listDocuments.useQuery();
 
-  return <div>{data}</div>;
+  return (
+    <div>
+      <Button> {data}</Button>
+    </div>
+  );
 }
