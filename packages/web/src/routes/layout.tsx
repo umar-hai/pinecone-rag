586c6bf6107df8fb069e9ac3abc0f14572ef7c7c
Umar Mohammed <umar.mohammed@holisticai.com>
2024-02-03 15:25:58 +0000

    style layout page

--- packages/web/src/routes/layout.tsx
+++ packages/web/src/routes/layout.tsx
@@ -1,5 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function Layout() {
-  return <Outlet />;
+  return (
+    <div className="flex-col md:flex">
+      <header className="border-b flex h-16 justify-between items-center px-4">
+        <div className="container mx-auto flex gap-2 items-center justify-between">
+          <div className="flex items-baseline">
+            <h1 className="text-2xl font-bold tracking-tight mr-6">
+              <Link to="/">pinecone rag</Link>
+            </h1>
+            <div className="flex gap-2"></div>
+          </div>
+        </div>
+      </header>
+      <main className="container mx-auto flex-1 pt-6 px-4">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
--- packages/web/src/routes/listDocuments.tsx
+++ packages/web/src/routes/listDocuments.tsx
@@ -1,7 +1,15 @@
 import { trpc } from "@/utils/trpc";
+import { Link } from "react-router-dom";
 
 export function ListDocuments() {
   const { data } = trpc.listDocuments.useQuery();
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div className="flex flex-col">
+      <Link className="hover:underline" to="create">
+        Add{" "}
+      </Link>
+      {JSON.stringify(data)}
+    </div>
+  );
 }
