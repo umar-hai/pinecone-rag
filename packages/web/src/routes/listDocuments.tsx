429aceb0b1f62cf36964720970e684e99a0ed832
Umar Mohammed <umar.mohammed@holisticai.com>
2024-02-04 11:49:12 +0000

    make the list documents page responsive

--- packages/web/src/routes/listDocuments.tsx
+++ packages/web/src/routes/listDocuments.tsx
@@ -10,7 +10,7 @@ export function ListDocuments() {
   }
 
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       {data.map((doc) => (
         <Card className="shadow-lg" key={doc.id}>
           <img
